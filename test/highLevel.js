86b66942064a2ce9905e6fac1c8c6159a5ef9b2d
Ra'Shaun Stovall <help@sneakyhead.com>
2016-08-31 21:14:21 -0400

    Update Test Struture (#21)

--- test/highLevel.js
+++ test/highLevel.js
@@ -1,23 +1,23 @@
-var test = require('tape');
 var semver = require('semver');
-var browser;
+var browser = require('../');
+var test = require('tape');
 
 test('can run detection', function(t) {
-  t.plan(1);
-  t.ok(browser = require('../'));
+  t.ok(browser, require.cache[require.resolve('../')]);
+  t.end();
 });
 
 test('name detected', function(t) {
-  t.plan(1);
   t.ok(browser.name, browser.name);
+  t.end();
 });
 
 test('version detected', function(t) {
-  t.plan(1);
   t.ok(browser.version, browser.version);
+  t.end();
 });
 
 test('version is semver compatible', function(t) {
-  t.plan(1);
   t.ok(semver.valid(browser.version));
+  t.end();
 });
