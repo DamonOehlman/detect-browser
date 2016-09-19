72c32070328220ae414699080d2b2b916355d056
Cortez <gsotirov@users.noreply.github.com>
2016-09-19 11:55:23 +0300

    Update logic.js
    ...

--- test/logic.js
+++ test/logic.js
@@ -21,7 +21,7 @@ test('detects Chrome', function(t) {
 
 test('detects Chrome for iOS', function(t) {
   assertAgentString(t,
-    Mozilla/5.0 (iPhone; U; CPU iPhone OS 5_1_1 like Mac OS X; en) AppleWebKit/534.46.0 (KHTML, like Gecko) CriOS/19.0.1084.60 Mobile/9B206 Safari/7534.48.3,
+    'Mozilla/5.0 (iPhone; U; CPU iPhone OS 5_1_1 like Mac OS X; en) AppleWebKit/534.46.0 (KHTML, like Gecko) CriOS/19.0.1084.60 Mobile/9B206 Safari/7534.48.3',
     { name: 'crios', version: '19.0.1084' }
   );
 
