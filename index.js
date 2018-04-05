99781f631410b6fac7f9c817634a4a667949afb1
Damon Oehlman <damon.oehlman@gmail.com>
2018-04-05 16:51:20 +1000

    prevent prettier running for now

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function detect() {
 
 function detectOS(userAgentString) {
   var rules = getOperatingSystemRules();
-  var detected = rules.filter(function(os) {
+  var detected = rules.filter(function (os) {
     return os.rule && os.rule.test(userAgentString);
   })[0];
 
@@ -20,15 +20,11 @@ function detectOS(userAgentString) {
 
 function getNodeVersion() {
   var isNode = typeof navigator === 'undefined' && typeof process !== 'undefined';
-  return isNode
-    ? {
-        name: 'node',
-        version: process.version.slice(1),
-        os: require('os')
-          .type()
-          .toLowerCase(),
-      }
-    : null;
+  return isNode ? {
+    name: 'node',
+    version: process.version.slice(1),
+    os: require('os').type().toLowerCase()
+  } : null;
 }
 
 function parseUserAgent(userAgentString) {
@@ -37,24 +33,19 @@ function parseUserAgent(userAgentString) {
     return null;
   }
 
-  var detected =
-    browsers
-      .map(function(browser) {
-        var match = browser.rule.exec(userAgentString);
-        var version = match && match[1].split(/[._]/).slice(0, 3);
+  var detected = browsers.map(function(browser) {
+    var match = browser.rule.exec(userAgentString);
+    var version = match && match[1].split(/[._]/).slice(0,3);
 
-        if (version && version.length < 3) {
-          version = version.concat(version.length == 1 ? [0, 0] : [0]);
-        }
+    if (version && version.length < 3) {
+      version = version.concat(version.length == 1 ? [0, 0] : [0]);
+    }
 
-        return (
-          match && {
-            name: browser.name,
-            version: version.join('.'),
-          }
-        );
-      })
-      .filter(Boolean)[0] || null;
+    return match && {
+      name: browser.name,
+      version: version.join('.')
+    };
+  }).filter(Boolean)[0] || null;
 
   if (detected) {
     detected.os = detectOS(userAgentString);
@@ -65,57 +56,57 @@ function parseUserAgent(userAgentString) {
 
 function getBrowserRules() {
   return buildRules([
-    ['aol', /AOLShield\/([0-9\._]+)/],
-    ['edge', /Edge\/([0-9\._]+)/],
-    ['yandexbrowser', /YaBrowser\/([0-9\._]+)/],
-    ['vivaldi', /Vivaldi\/([0-9\.]+)/],
-    ['kakaotalk', /KAKAOTALK\s([0-9\.]+)/],
-    ['chrome', /(?!Chrom.*OPR)Chrom(?:e|ium)\/([0-9\.]+)(:?\s|$)/],
-    ['phantomjs', /PhantomJS\/([0-9\.]+)(:?\s|$)/],
-    ['crios', /CriOS\/([0-9\.]+)(:?\s|$)/],
-    ['firefox', /Firefox\/([0-9\.]+)(?:\s|$)/],
-    ['fxios', /FxiOS\/([0-9\.]+)/],
-    ['opera', /Opera\/([0-9\.]+)(?:\s|$)/],
-    ['opera', /OPR\/([0-9\.]+)(:?\s|$)$/],
-    ['ie', /Trident\/7\.0.*rv\:([0-9\.]+).*\).*Gecko$/],
-    ['ie', /MSIE\s([0-9\.]+);.*Trident\/[4-7].0/],
-    ['ie', /MSIE\s(7\.0)/],
-    ['bb10', /BB10;\sTouch.*Version\/([0-9\.]+)/],
-    ['android', /Android\s([0-9\.]+)/],
-    ['ios', /Version\/([0-9\._]+).*Mobile.*Safari.*/],
-    ['safari', /Version\/([0-9\._]+).*Safari/],
-    ['facebook', /FBAV\/([0-9\.]+)/],
-    ['instagram', /Instagram\ ([0-9\.]+)/],
+    [ 'aol', /AOLShield\/([0-9\._]+)/ ],
+    [ 'edge', /Edge\/([0-9\._]+)/ ],
+    [ 'yandexbrowser', /YaBrowser\/([0-9\._]+)/ ],
+    [ 'vivaldi', /Vivaldi\/([0-9\.]+)/ ],
+    [ 'kakaotalk', /KAKAOTALK\s([0-9\.]+)/ ],
+    [ 'chrome', /(?!Chrom.*OPR)Chrom(?:e|ium)\/([0-9\.]+)(:?\s|$)/ ],
+    [ 'phantomjs', /PhantomJS\/([0-9\.]+)(:?\s|$)/ ],
+    [ 'crios', /CriOS\/([0-9\.]+)(:?\s|$)/ ],
+    [ 'firefox', /Firefox\/([0-9\.]+)(?:\s|$)/ ],
+    [ 'fxios', /FxiOS\/([0-9\.]+)/ ],
+    [ 'opera', /Opera\/([0-9\.]+)(?:\s|$)/ ],
+    [ 'opera', /OPR\/([0-9\.]+)(:?\s|$)$/ ],
+    [ 'ie', /Trident\/7\.0.*rv\:([0-9\.]+).*\).*Gecko$/ ],
+    [ 'ie', /MSIE\s([0-9\.]+);.*Trident\/[4-7].0/ ],
+    [ 'ie', /MSIE\s(7\.0)/ ],
+    [ 'bb10', /BB10;\sTouch.*Version\/([0-9\.]+)/ ],
+    [ 'android', /Android\s([0-9\.]+)/ ],
+    [ 'ios', /Version\/([0-9\._]+).*Mobile.*Safari.*/ ],
+    [ 'safari', /Version\/([0-9\._]+).*Safari/ ],
+    [ 'facebook', /FBAV\/([0-9\.]+)/],
+    [ 'instagram', /Instagram\ ([0-9\.]+)/]
   ]);
 }
 
 function getOperatingSystemRules() {
   return buildRules([
-    ['iOS', /iP(hone|od|ad)/],
-    ['Android OS', /Android/],
-    ['BlackBerry OS', /BlackBerry|BB10/],
-    ['Windows Mobile', /IEMobile/],
-    ['Amazon OS', /Kindle/],
-    ['Windows 3.11', /Win16/],
-    ['Windows 95', /(Windows 95)|(Win95)|(Windows_95)/],
-    ['Windows 98', /(Windows 98)|(Win98)/],
-    ['Windows 2000', /(Windows NT 5.0)|(Windows 2000)/],
-    ['Windows XP', /(Windows NT 5.1)|(Windows XP)/],
-    ['Windows Server 2003', /(Windows NT 5.2)/],
-    ['Windows Vista', /(Windows NT 6.0)/],
-    ['Windows 7', /(Windows NT 6.1)/],
-    ['Windows 8', /(Windows NT 6.2)/],
-    ['Windows 8.1', /(Windows NT 6.3)/],
-    ['Windows 10', /(Windows NT 10.0)/],
-    ['Windows ME', /Windows ME/],
-    ['Open BSD', /OpenBSD/],
-    ['Sun OS', /SunOS/],
-    ['Linux', /(Linux)|(X11)/],
-    ['Mac OS', /(Mac_PowerPC)|(Macintosh)/],
-    ['QNX', /QNX/],
-    ['BeOS', /BeOS/],
-    ['OS/2', /OS\/2/],
-    ['Search Bot', /(nuhk)|(Googlebot)|(Yammybot)|(Openbot)|(Slurp)|(MSNBot)|(Ask Jeeves\/Teoma)|(ia_archiver)/],
+    [ 'iOS', /iP(hone|od|ad)/ ],
+    [ 'Android OS', /Android/ ],
+    [ 'BlackBerry OS', /BlackBerry|BB10/ ],
+    [ 'Windows Mobile', /IEMobile/ ],
+    [ 'Amazon OS', /Kindle/ ],
+    [ 'Windows 3.11', /Win16/ ],
+    [ 'Windows 95', /(Windows 95)|(Win95)|(Windows_95)/ ],
+    [ 'Windows 98', /(Windows 98)|(Win98)/ ],
+    [ 'Windows 2000', /(Windows NT 5.0)|(Windows 2000)/ ],
+    [ 'Windows XP', /(Windows NT 5.1)|(Windows XP)/ ],
+    [ 'Windows Server 2003', /(Windows NT 5.2)/ ],
+    [ 'Windows Vista', /(Windows NT 6.0)/ ],
+    [ 'Windows 7', /(Windows NT 6.1)/ ],
+    [ 'Windows 8', /(Windows NT 6.2)/ ],
+    [ 'Windows 8.1', /(Windows NT 6.3)/ ],
+    [ 'Windows 10', /(Windows NT 10.0)/ ],
+    [ 'Windows ME', /Windows ME/ ],
+    [ 'Open BSD', /OpenBSD/ ],
+    [ 'Sun OS', /SunOS/ ],
+    [ 'Linux', /(Linux)|(X11)/ ],
+    [ 'Mac OS', /(Mac_PowerPC)|(Macintosh)/ ],
+    [ 'QNX', /QNX/ ],
+    [ 'BeOS', /BeOS/ ],
+    [ 'OS/2', /OS\/2/ ],
+    [ 'Search Bot', /(nuhk)|(Googlebot)|(Yammybot)|(Openbot)|(Slurp)|(MSNBot)|(Ask Jeeves\/Teoma)|(ia_archiver)/ ]
   ]);
 }
 
@@ -123,7 +114,7 @@ function buildRules(ruleTuples) {
   return ruleTuples.map(function(tuple) {
     return {
       name: tuple[0],
-      rule: tuple[1],
+      rule: tuple[1]
     };
   });
 }
@@ -132,5 +123,5 @@ module.exports = {
   detect: detect,
   detectOS: detectOS,
   getNodeVersion: getNodeVersion,
-  parseUserAgent: parseUserAgent,
+  parseUserAgent: parseUserAgent
 };
